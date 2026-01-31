4cfea99cb6ba2171617656d6daa979dac0134396
SaumyaPatidaar <gsssmaheshpurabsw@gmail.com>
2026-01-31 17:03:44 +0530

    Update start.py
    Signed-off-by: SaumyaPatidaar <gsssmaheshpurabsw@gmail.com>

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,13 +26,13 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_6"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="• khargosh •", user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_5"], url=config.SUPPORT_CHANNEL),
         ],
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/xbitcode/music.git")],
+        [InlineKeyboardButton(text="• sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ •", url=f"https://github.com/SaumyaPatidaar/music")],
     
     ]
     
